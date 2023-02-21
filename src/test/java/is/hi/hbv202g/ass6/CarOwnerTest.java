ae6e66c870a336171d8c12efbf4bbea31c44b813
Fergal Riordan <riordanf@tcd.ie>
2023-02-21 11:06:30 +0000

    Step 6 complete: refactored test classes

--- src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
@@ -1,16 +1,23 @@
 package is.hi.hbv202g.ass6;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.*;
 
 public class CarOwnerTest {
 
+    private Car car;
+    private CarOwner carOwner;
+
     @Test
     public void getName() {
-        Car car=new Car("Beetle");
-        CarOwner carOwner = new CarOwner("John Doe", car);
         assertEquals("John Doe", carOwner.getName());
     }
 
+    @Before 
+    public void setUp() {
+        car = new Car("Beetle");
+        carOwner = new CarOwner("John Doe", car);
+    }
 }
--- src/test/java/is/hi/hbv202g/ass6/CarTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarTest.java
@@ -1,15 +1,21 @@
 package is.hi.hbv202g.ass6;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
 public class CarTest {
+    private Car car;
+
     @Test
     public void getNameOfCar() {
-        Car car=new Car("Beetle");
-        //CarOwner carOwner = new CarOwner("John Doe", car);
         assertEquals("Beetle", car.getName());
+    }
+
+    @Before
+    public void setUp() {
+        car = new Car("Beetle");
     }
 
 }
